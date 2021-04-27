eeff0d61083f0df0ce7e0fb326c176fe2b92d6b9
Sanzay Dahal <sanzaydahal12345@gmail.com>
2021-04-27 18:40:28 +1000

    ChatPage groupImages display logic and style Images

--- client/src/pages/messages/chat/ChatPage.tsx
+++ client/src/pages/messages/chat/ChatPage.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 
 import * as chatActions from '../../../store/actions/chat/chatActions';
+import { UserType } from '../../../store/actions/user/userActionTypes';
 import { RootStore } from '../../../store/store';
 
 import './chat.scss';
@@ -11,6 +12,23 @@ interface RouteParams {
   id: string;
 }
 
+const maxImagesToShow = 3;
+
+const getTitleBarImages = (users: UserType[], loggedInUser: UserType) => {
+  let images: string[] = [];
+
+  if (users.length === 1) {
+    images.push(users[0].profilePic);
+    return images;
+  }
+  const otherUsers = users.filter((u) => u._id !== loggedInUser._id);
+  return otherUsers.slice(0, maxImagesToShow).map((u) => u.profilePic);
+};
+
+const getRemainingImagesCount = (totalCount: number) => {
+  return totalCount - maxImagesToShow - 1; // Remove 1 since we won't show our own image
+};
+
 const ChatPage: React.FC<RouteComponentProps<RouteParams>> = ({ match }) => {
   const chatId = match.params.id;
 
@@ -18,16 +36,40 @@ const ChatPage: React.FC<RouteComponentProps<RouteParams>> = ({ match }) => {
 
   const state = useSelector((state: RootStore) => state);
 
+  const loggedInUser = state.loggedInUserInfo.user!;
+
   const chatDetailsState = state.chatDetails;
-  const { chat } = chatDetailsState;
+  const { chat, loading, error } = chatDetailsState;
 
   useEffect(() => {
     dispatch(chatActions.getChatDetails(chatId));
   }, [dispatch, chatId]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  } else if (error) {
+    return <p>{error}</p>;
+  } else if (!chat) {
+    return <p>No Chat Found</p>;
+  }
+
+  const images = getTitleBarImages(chat.users, loggedInUser);
+  const remainingImagesCount = getRemainingImagesCount(chat.users.length);
+
   return (
     <div className='chatPageContainer'>
       <div className='chatTitleBarContianer'>
-        <span id='chatName'>{chat ? chat._id : 'This is the chat'}</span>
+        <div className='chatImagesContainer'>
+          {remainingImagesCount > 0 && (
+            <div className='userCount'>
+              <span>+{remainingImagesCount}</span>
+            </div>
+          )}
+          {images.map((src, i) => (
+            <img key={i} src={src} alt={'User' + i} />
+          ))}
+        </div>
+        <span id='chatName'>{chat._id}</span>
       </div>
       <div className='mainContentContainer'>
         <div className='chatContainer'>
